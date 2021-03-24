cbe3512a16ecdf2ce714429ac9dfd8286535dbfd
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-03-23 23:26:43 -0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+city_name = input("What's the name of the city you grew up in:\n")
+pet_name = input("What was the name of your first pet?\n")
+
+print("You should call your band: " + city_name.capitalize() + " " + pet_name.capitalize())
